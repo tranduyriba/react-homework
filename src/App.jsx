95dddf774a35fc518472c4ac6e43f3cc272e8cc8
Duy Riba <tranduy.riba@gmail.com>
2024-11-21 23:21:00 +0700

    Fix CSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ProductsList from "./components/products";
 import Footer from "./components/footer";
 import Header from "./components/header";
+import App from './App';
 
 function App() {
   return (
